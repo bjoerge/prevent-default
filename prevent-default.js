77afd320875be2bf4489577c03640c45a3158a77
Bjørge Næss <bjoerge@bengler.no>
2013-06-10 17:05:29 +0200

    Simplify

--- prevent-default.js
+++ prevent-default.js
@@ -1,10 +1,6 @@
-
-var slice = Array.prototype.slice;
-
 module.exports = function preventDefault(listener) {
-  return function(e) {
+  return function(ev) {
     e.preventDefault();
-    var ret = listener.apply(this, slice.call(arguments));
-    return ret === false ? false : ret;
+    return listener.call(this, ev) === true;
   }
 };
